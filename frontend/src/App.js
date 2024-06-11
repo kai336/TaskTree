9c835c44ca66186a8b51dc25c7ad4db537d600e2
kai336 <shiturukai.336@gmail.com>
2024-06-11 19:04:22 +0900

    add convert function (json data from DB to List[Task])

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
 import { useState, useRef, useEffect } from "react";
-import {v4 as uuidv4} from "uuid"
-import Trees from "./components/Trees"
+import { v4 as uuidv4 } from "uuid";
+import {dataToTaskTrees} from "./components/ConvertJsonToTaskTrees";
+import Trees from "./components/Trees";
 import ButtonAppBar from "./components/Header";
-import "./css/App.css"
+import "./css/App.css";
 import { Button, Container, Checkbox, TextField, Box, Stack, createTheme, ThemeProvider, CssBaseline } from "@mui/material";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from "dayjs";
 import { jaJP } from "@mui/x-date-pickers/locales";
 
-dayjs.locale(jaJP); 
+dayjs.locale(jaJP);
 
-class Task {
+export class Task {
   constructor(id, text, deadline, completed, root, children) {
     this.id = id;
     this.text = text;
@@ -19,8 +20,8 @@ class Task {
     this.completed = completed;
     this.root = root;
     this.children = children;
-  }
-}
+  };
+};
 
 const darkTheme = createTheme({
   palette: {
@@ -28,62 +29,22 @@ const darkTheme = createTheme({
   },
 });
 
-// todo: add task finish animation
-// todo: implement backend
 
-// convert json to single task
-const dataToSingleTask = (data, children) => {
-  return new Task(
-    data.task_id,
-    data.description,
-    data.deadline,
-    data.completed,
-    data.root_id,
-    children
-  )
-}
+/*
+todo: add task finish animation
+todo: implement backend
+      - reflect changes on task trees
+        - build api
+          - add child task
+            => add child task to DB
+            => push back child's id to parent task's children[]
 
-// convert json to root tasks
-const dataToTaskRoots = (datas) => {
-  const rootTasks = datas
-  .filter(data => {
-    return data.task_id === data.root_id // filter root task
-  })
-  .map(data => { // make task
-    return new Task(
-      data.task_id,
-      data.description,
-      data.deadline,
-      data.completed,
-      data.root_id,
-      [] // temporaly initialize children
-    );
-  });
-  return rootTasks;
-};
+          - add root task
+            => add root task to DB
 
-// wip
-// get children tasks and build task tree
-const taskRootsToTaskTrees = (taskRoots, datas) => {
-  const taskTrees = taskRoots.map(rootTask => { // for all root tasks
-    const rootData = datas.find(data => data.task_id === rootTask.task_id); // get the data of root task
-    if(!rootData) {
-      return null; // return null if root task is not found
-    };
-    const getChildTasks = (taskId) => { // return array of child tasks
-      const parentTask = datas.find(data => data.task_id === taskId); // find parent task from id
-      const chidlTasks = parentTask.children.map(childId => { // for all children
-        const childData = datas.find(data => data.task_id === childId);
-        if(!childData) {
-          return null; // if child was not found, return null
-        };
-        return dataToSingleTask(childData, []);
-      });
-    };
-    return dataToSingleTask(rootTask, getChildTasks(rootTask));
-  });
-  return taskTrees;
-};
+          after each operation, reflesh data and render screen
+
+*/
 
 function App() {
   const [taskTrees, setTaskTrees] = useState([]);
@@ -114,12 +75,15 @@ function App() {
     fetchData();
   }, []); // 空の依存配列により、コンポーネントのマウント時にのみ実行されます
 
-  if(data.length != 0){
-    console.log(dataToTaskRoots(data));
-    const rootTasks = dataToTaskRoots(data);
-    const Tasks = taskRootsToTaskTrees(rootTasks, data);
-    console.log(Tasks); // not working
-  }
+  useEffect(() => {
+    if (data.length != 0) {
+      const usersTaskTrees = dataToTaskTrees(data);
+      setTaskTrees(usersTaskTrees);
+      console.log(taskTrees);
+    }
+  }, [data]);
+
+
 
   // add root task
   const addRootTask = () => {
@@ -146,13 +110,13 @@ function App() {
     setShowForm(!showForm);
   };
 
-  
+
   // insert child task
   const updateTaskTree = (currentNode, id, newTask) => {
-    if(currentNode.id === id){
+    if (currentNode.id === id) {
       console.log('fonud');
       currentNode.children.push(newTask);
-    }else if(currentNode.children){
+    } else if (currentNode.children) {
       currentNode.children = currentNode.children.map((childNode) => {
         return updateTaskTree(childNode, id, newTask);
       });
@@ -163,35 +127,35 @@ function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-        <Container maxWidth='sm'>
-          <ButtonAppBar />
-          <Stack direction='column' sx={{my:2}} spacing={2}>
-            <Button onClick={handleButtonClick}>親タスクを追加</Button>
-            {showForm && (
-              <Stack direction='row' sx={{my:2}} spacing={1}>
-                <TextField
-                  label='内容'
-                  varriant='standard-basic'
-                  value={taskText}
-                  onChange={(newText) => setTaskText(newText.target.value)}
-                />
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
+      <Container maxWidth='sm'>
+        <ButtonAppBar />
+        <Stack direction='column' sx={{ my: 2 }} spacing={2}>
+          <Button onClick={handleButtonClick}>親タスクを追加</Button>
+          {showForm && (
+            <Stack direction='row' sx={{ my: 2 }} spacing={1}>
+              <TextField
+                label='内容'
+                varriant='standard-basic'
+                value={taskText}
+                onChange={(newText) => setTaskText(newText.target.value)}
+              />
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
                   label='日付'
                   value={taskDate}
                   onChange={(newDate) => setTaskDate(newDate)}
                 />
-                </LocalizationProvider>
-                <Button onClick={addRootTask}>追加</Button>
-              </Stack>
-            )}
-            <Trees taskTrees={taskTrees} addChildTask={addChildTask} />
-          </Stack>
+              </LocalizationProvider>
+              <Button onClick={addRootTask}>追加</Button>
+            </Stack>
+          )}
+          <Trees taskTrees={taskTrees} addChildTask={addChildTask} />
+        </Stack>
       </Container>
     </ThemeProvider>
-    
+
   );
-    
+
 }
 
 export default App;
--- postgres/init.sql
+++ postgres/init.sql
@@ -12,17 +12,22 @@ CREATE TABLE task_tree (
     completed BOOLEAN DEFAULT false
 );
 
+-- add root task 1
 INSERT INTO task_tree (user_id, task_id, root_id, description, deadline, completed)
     VALUES ('d03cc72c-a129-49a2-a7b7-fa9097bab9e6', 'ed011eb7-bcca-4746-a739-4979eafed2b9', 'ed011eb7-bcca-4746-a739-4979eafed2b9', 'this is a test root1', '2024-06-10', false);
 
+-- add child task 1 to root task 1
+INSERT INTO task_tree (user_id, task_id, root_id, description, deadline, completed)
+    VALUES ('d03cc72c-a129-49a2-a7b7-fa9097bab9e6', '8ee874e6-a5c7-4b4e-83ea-b5a7a4bd2904', 'ed011eb7-bcca-4746-a739-4979eafed2b9', 'this is a test children1', '2024-06-10', false);
 UPDATE task_tree SET children = COALESCE(children, ARRAY[]::UUID[]) || '{8ee874e6-a5c7-4b4e-83ea-b5a7a4bd2904}'
 WHERE user_id = 'd03cc72c-a129-49a2-a7b7-fa9097bab9e6' AND task_id = 'ed011eb7-bcca-4746-a739-4979eafed2b9';
 
-INSERT INTO task_tree (user_id, task_id, root_id, description, deadline, completed)
-    VALUES ('d03cc72c-a129-49a2-a7b7-fa9097bab9e6', '8ee874e6-a5c7-4b4e-83ea-b5a7a4bd2904', 'ed011eb7-bcca-4746-a739-4979eafed2b9', 'this is a test children1', '2024-06-10', false);
-
+-- add child task 2 to root task 1
 INSERT INTO task_tree (user_id, task_id, root_id, description, deadline, completed)
     VALUES ('d03cc72c-a129-49a2-a7b7-fa9097bab9e6', 'c583ee3f-616a-4ea2-b6d4-0c4df790ae5e', 'ed011eb7-bcca-4746-a739-4979eafed2b9', 'this is a test children2', '2024-06-10', false);
+UPDATE task_tree SET children = COALESCE(children, ARRAY[]::UUID[]) || '{c583ee3f-616a-4ea2-b6d4-0c4df790ae5e}'
+WHERE user_id = 'd03cc72c-a129-49a2-a7b7-fa9097bab9e6' AND task_id = 'ed011eb7-bcca-4746-a739-4979eafed2b9';
 
+-- add root task 2
 INSERT INTO task_tree (user_id, task_id, root_id, description, deadline, completed)
     VALUES ('d03cc72c-a129-49a2-a7b7-fa9097bab9e6', '4c46160e-2874-4f8f-99b2-1b4dd3b3e89a', '4c46160e-2874-4f8f-99b2-1b4dd3b3e89a', 'this is a test root2', '2024-06-10', false);
